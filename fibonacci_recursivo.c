f3574010e7cb2b2bd47722a622d1672240cb8056
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-05-09 15:17:52 -0300

    Atualizar o fibonacci_recursivo.c

--- fibonacci_recursivo.c
+++ fibonacci_recursivo.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 int fibonacci(int posicao) {
 
     if (posicao == 1) {
         return 0;
-
     }
-
     if (posicao == 2)
     {
         return 1;
-    }
-    
+    }   
     return fibonacci(posicao - 2) + fibonacci(posicao - 1);
 
 }
-
 
 int main(int argc, char const *argv[])
 {
@@ -27,12 +21,9 @@ int main(int argc, char const *argv[])
 
     printf("Digite um numero: ");
     scanf("%i", &valor);
-
     
     aux = fibonacci(valor);
     
-
-
     system("pause");
     return 0;
 }
